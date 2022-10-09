21bbccfd7824619b91a37abee6539f5d6d592746
Aurelien Labrosse <aurelien@skima.fr>
2022-10-10 00:33:17 +0200

    chore: remove useless files

--- test/embedded/unity_config.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-#define UNITY_OUTPUT_CHAR(a)    Serial.print(a)
-#define UNITY_OUTPUT_START()    Serial.begin(115200)
-#define UNITY_OUTPUT_FLUSH()    RS232_flush()
-#define UNITY_OUTPUT_COMPLETE()
