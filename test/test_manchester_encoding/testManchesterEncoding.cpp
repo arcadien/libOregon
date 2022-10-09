e420f443164286c35b23cb405aef66eba698dc98
Aurelien Labrosse <aurelien@skima.fr>
2022-10-09 22:49:37 +0200

    test: implemented test HAL for native environment

--- test/test_manchester_encoding/testManchesterEncoding.cpp
+++ test/test_manchester_encoding/testManchesterEncoding.cpp
@@ -1,11 +1,60 @@
 #include <unity.h>
-
+#include <string.h>
 #if defined(AVR)
 #include <Arduino.h>
 #endif
 
+#include <oregon.h>
+
+class TestHal : public Hal
+{
+public:
+  TestHal() : Hal()
+  {
+    reset();
+  }
+
+  void reset()
+  {
+    radioOutputIndex = 0;
+    memset(&radioOutput[0], 0, (128 / sizeof(int)));
+  }
+
+  void rfHigh()
+  {
+    radioOutput[radioOutputIndex] = 'H';
+    radioOutputIndex++;
+  }
+  void rfLow()
+  {
+    radioOutput[radioOutputIndex] = 'L';
+    radioOutputIndex++;
+  }
+
+  void shortWait()
+  {
+    radioOutput[radioOutputIndex] = 'S';
+    radioOutputIndex++;
+  }
+
+  void longWait()
+  {
+    radioOutput[radioOutputIndex] = 'W';
+    radioOutputIndex++;
+  }
+
+  char radioOutput[128];
+
+private:
+  uint8_t radioOutputIndex;
+};
+
+TestHal testHal;
+
+Oregon oregon(testHal);
 void setUp(void)
 {
+  testHal.reset();
 }
 
 void tearDown(void)
