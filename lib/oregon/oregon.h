2961c0812131d4b6311618ecc24f4ce4cf98284c
Aurelien Labrosse <aurelien@skima.fr>
2022-10-11 00:43:27 +0200

    feat: Implement Oregon#sendData()

--- lib/oregon/oregon.h
+++ lib/oregon/oregon.h
@@ -33,12 +33,35 @@ public:
         _hal->rfHigh();
         _hal->shortWait();
     }
-   
-    void sendData(const unsigned char* data, uint8_t size)
+
+    void sendData(const uint8_t *data, uint8_t size)
     {
-        
+        for (uint8_t i = 0; i < size; i++)
+        {
+            sendMSB(data[i]);
+            sendLSB(data[i]);
+        }
     }
 
 private:
+#ifndef bitread
+#define bitRead(value, bit) (((value) >> (bit)) & 0x01)
+#endif
+    void sendMSB(uint8_t data)
+    {
+        (bitRead(data, 4)) ? sendOne() : sendZero();
+        (bitRead(data, 5)) ? sendOne() : sendZero();
+        (bitRead(data, 6)) ? sendOne() : sendZero();
+        (bitRead(data, 7)) ? sendOne() : sendZero();
+    }
+
+    void sendLSB(uint8_t data)
+    {
+        (bitRead(data, 0)) ? sendOne() : sendZero();
+        (bitRead(data, 1)) ? sendOne() : sendZero();
+        (bitRead(data, 2)) ? sendOne() : sendZero();
+        (bitRead(data, 3)) ? sendOne() : sendZero();
+    }
+
     Hal *_hal;
 };
