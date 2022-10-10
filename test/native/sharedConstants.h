a45d500563fbcccd23cf533ec1b3baefb041b2ce
Aurelien Labrosse <aurelien@skima.fr>
2022-10-10 23:59:13 +0200

    chore: extract constants from test

---
+++ test/native/sharedConstants.h
@@ -0,0 +1,9 @@
+#pragma once
+
+static const uint8_t CHAR_COUNT_FOR_A_BIT = 6;
+
+static const unsigned char
+    EXPECTED_ORDERS_FOR_ZERO[CHAR_COUNT_FOR_A_BIT] = {'H', 'S', 'L', 'W', 'H', 'S'};
+
+static const unsigned char
+    EXPECTED_ORDERS_FOR_ONE[CHAR_COUNT_FOR_A_BIT] = {'L', 'S', 'H', 'W', 'L', 'S'};
--- test/native/test_manchester_encoding/testManchesterEncoding.cpp
+++ test/native/test_manchester_encoding/testManchesterEncoding.cpp
@@ -3,6 +3,7 @@
 #include <unity.h>
 #include <string.h>
 #include <oregon.h>
+#include <sharedConstants.h>
 
 class NativeHal : public Hal
 {
@@ -59,14 +60,6 @@ void setUp(void)
 void tearDown(void)
 {
 }
-
-static const uint8_t CHAR_COUNT_FOR_A_BIT = 6;
-
-static const unsigned char
-    EXPECTED_ORDERS_FOR_ZERO[CHAR_COUNT_FOR_A_BIT] = {'H', 'S', 'L', 'W', 'H', 'S'};
-
-static const unsigned char
-    EXPECTED_ORDERS_FOR_ONE[CHAR_COUNT_FOR_A_BIT] = {'L', 'S', 'H', 'W', 'L', 'S'};
 
 void expect_encoding_zero()
 {
